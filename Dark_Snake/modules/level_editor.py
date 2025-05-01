71a19a13d2eba068f00e0f50ed44bd31ac18d096
DemusOpalium <majordemus@yahoo.de>
2025-05-01 21:16:58 +0200

    Update level_editor.py (#84)
    Fix Editor New Tiles.
     alpha.015
    
    Signed-off-by: DemusOpalium <majordemus@yahoo.de>

--- Dark_Snake/modules/level_editor.py
+++ Dark_Snake/modules/level_editor.py
@@ -1,26 +1,23 @@
-# modules/level_editor.py – FINAL UI-STRUKTUR
-# ✔ Favoritenzeile oben
-# ✔ Containertrennung: Buttons, Favoriten, Tiles, Karte
-# ✔ Kein Überlappen mehr
-# ✔ Maus-Zeichnen
-# ✔ Debugausgaben für alle UI-Aktionen
-
+# modules/level_editor.py – Finales UI-Layout mit breiterem, tieferem Tile-Container
 import os, json, pygame
 from config import GRID_WIDTH, GRID_HEIGHT, GRID_SIZE, WINDOW_WIDTH, WINDOW_HEIGHT
 
-TOOLBAR_W = 180
-BTN_H     = 28
-ZOOM      = 0.72
+TOOLBAR_W = 200
+BTN_H = 28
+ZOOM = 0.72
 FAV_ROW_H = 50
-TILE_ROW_H = 250
-MAP_TOP   = FAV_ROW_H + TILE_ROW_H - 40  # ⬅ Spielfeld höher angesetzt, Abstand nach unten entsteht
-MAX_FAV   = 8
+TILE_ROW_H = 600
+SCROLL_TOP = 275
+MAP_TOP = SCROLL_TOP + TILE_ROW_H - 650  # damit Spielfeld darunter bleibt
+MAX_FAV = 8
+
 
 COLOR_BG     = (25, 25, 25)
 COLOR_BTN    = (50, 50, 50)
 COLOR_FAV    = (70, 70, 70)
 COLOR_TEXT   = (255, 255, 255)
 COLOR_SELECT = (255, 255, 0)
+COLOR_FRAME  = (120, 0, 120)
 
 class LevelEditor:
     def __init__(self, game):
@@ -37,6 +34,7 @@ class LevelEditor:
         self.font = pygame.font.SysFont("Arial", 15)
         self.info = ""
         self.buttons = self._make_buttons()
+        self.tile_scroll_offset = 0
 
     def _load_tiles(self):
         from modules.graphics import load_image
@@ -80,18 +78,22 @@ class LevelEditor:
         if not self.active: return
         if event.type == pygame.MOUSEBUTTONDOWN:
             mx, my = pygame.mouse.get_pos()
-            if mx < TOOLBAR_W and my < FAV_ROW_H + TILE_ROW_H:
+            if mx < TOOLBAR_W and my < SCROLL_TOP:
                 for _, rect, action in self.buttons:
                     if rect.collidepoint((mx, my)):
                         action()
                         return
-            elif mx > TOOLBAR_W:
+            elif mx > TOOLBAR_W or my > SCROLL_TOP:
                 self.drawing = True
                 self._paint(mx, my)
         elif event.type == pygame.MOUSEBUTTONUP:
             self.drawing = False
         elif event.type == pygame.MOUSEMOTION and self.drawing:
             self._paint(*event.pos)
+        elif event.type == pygame.MOUSEWHEEL:
+            max_scroll = max(0, (len(self.tiles) // 5 - 6) * (GRID_SIZE + 4))
+            self.tile_scroll_offset += event.y * 20
+            self.tile_scroll_offset = max(-max_scroll, min(0, self.tile_scroll_offset))
 
     def _paint(self, mx, my):
         gx = int((mx - TOOLBAR_W) / (GRID_SIZE * ZOOM))
@@ -111,7 +113,6 @@ class LevelEditor:
             text = self.font.render(label, True, COLOR_TEXT)
             screen.blit(text, (rect.x + 6, rect.y + 6))
 
-        # Favoritenbereich
         pygame.draw.rect(screen, COLOR_FAV, (TOOLBAR_W, 0, WINDOW_WIDTH - TOOLBAR_W, FAV_ROW_H))
         for i, key in enumerate(self.favorites):
             img = self.tiles.get(key)
@@ -121,33 +122,37 @@ class LevelEditor:
                 screen.blit(img, (x, y))
                 pygame.draw.rect(screen, COLOR_SELECT, (x, y, GRID_SIZE, GRID_SIZE), 1)
 
-        # Tile-Palette (max 2 Spalten)
+        scroll_area = pygame.Rect(0, SCROLL_TOP, TOOLBAR_W, TILE_ROW_H)
+        pygame.draw.rect(screen, (40, 40, 40), scroll_area)
+        pygame.draw.rect(screen, COLOR_FRAME, scroll_area, 2)
+        screen.set_clip(scroll_area)
+
         keys = list(self.tiles.keys())
         col_count = 5
         for i, key in enumerate(keys):
             col = i % col_count
             row = i // col_count
             x = 10 + col * (GRID_SIZE + 6)
-            y = FAV_ROW_H + TILE_ROW_H + 10 + row * (GRID_SIZE + 4)
+            y = SCROLL_TOP + 10 + row * (GRID_SIZE + 4) + self.tile_scroll_offset
             img = self.tiles[key]
             screen.blit(img, (x, y))
-            if key == self.current_tile:
-                pygame.draw.rect(screen, COLOR_SELECT, (x, y, GRID_SIZE, GRID_SIZE), 2)
             rect = pygame.Rect(x, y, GRID_SIZE, GRID_SIZE)
+            if key == self.current_tile:
+                pygame.draw.rect(screen, COLOR_SELECT, rect, 2)
             if rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
                 self.current_tile = key
             if rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[2]:
                 if key not in self.favorites and len(self.favorites) < MAX_FAV:
                     self.favorites.append(key)
                     self.info = f"[DEBUG] {key} zu Favoriten hinzugefügt."
+        screen.set_clip(None)
 
-        # Spielfeld
-        surf = pygame.Surface((GRID_WIDTH*GRID_SIZE, GRID_HEIGHT*GRID_SIZE), pygame.SRCALPHA)
+        surf = pygame.Surface((GRID_WIDTH * GRID_SIZE, GRID_HEIGHT * GRID_SIZE), pygame.SRCALPHA)
         for y, row in enumerate(self.map):
             for x, tile in enumerate(row):
                 if tile:
-                    surf.blit(self.tiles[tile], (x*GRID_SIZE, y*GRID_SIZE))
-        scaled = pygame.transform.scale(surf, (int(GRID_WIDTH*GRID_SIZE*ZOOM), int(GRID_HEIGHT*GRID_SIZE*ZOOM)))
+                    surf.blit(self.tiles[tile], (x * GRID_SIZE, y * GRID_SIZE))
+        scaled = pygame.transform.scale(surf, (int(GRID_WIDTH * GRID_SIZE * ZOOM), int(GRID_HEIGHT * GRID_SIZE * ZOOM)))
         screen.blit(scaled, (TOOLBAR_W, MAP_TOP))
 
         txt = self.font.render(self.info, True, COLOR_SELECT)
@@ -178,7 +183,7 @@ class LevelEditor:
     def set_border(self):
         for y in range(GRID_HEIGHT):
             for x in range(GRID_WIDTH):
-                if x == 0 or y == 0 or x == GRID_WIDTH-1 or y == GRID_HEIGHT-1:
+                if x == 0 or y == 0 or x == GRID_WIDTH - 1 or y == GRID_HEIGHT - 1:
                     self.map[y][x] = self.current_tile
         self.info = "Rand gesetzt"
 
